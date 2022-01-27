c1cd51c51411e7aeb68ff8df9049a6780d2a19e9
Trickym <pm21641@gmail.com>
2022-01-27 15:18:12 +0530

    catalogue

--- test_tut/lib/models/products.dart
+++ test_tut/lib/models/products.dart
@@ -13,17 +13,25 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image
+      };
 }
 
 class ProductModel {
-  static final products = [
-    Item(
-        id: 1,
-        name: "Product1",
-        desc: "This is product 1!",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
+  static var products = [];
 }
--- test_tut/lib/pages/home.dart
+++ test_tut/lib/pages/home.dart
@@ -1,10 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:test_tut/models/products.dart';
 import 'package:test_tut/widgets/drawer.dart';
 import 'package:test_tut/widgets/product_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final dummyList = List.generate(50, (index) => ProductModel.products[0]);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final productList =
+        await rootBundle.loadString("assets/files/products.json");
+    final decodedData = jsonDecode(productList);
+    final productData = decodedData["products"];
+    ProductModel.products =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +43,16 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) => ProductWidget(
-            item: dummyList[index],
-          ),
-        ),
+        child: ProductModel.products != null && ProductModel.products.isNotEmpty
+            ? ListView.builder(
+                itemCount: ProductModel.products.length,
+                itemBuilder: (context, index) => ProductWidget(
+                  item: ProductModel.products[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: const AppDrawer(),
     );
